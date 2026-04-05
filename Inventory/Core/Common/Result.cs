26c2721007de6c891388d49dfa19e13406b53ac5
mutassem-almahamid1 <mutassemalmahamid@gmail.com>
2026-04-06 02:50:59 +0300

    Add Result and Result<T> classes

---
+++ Inventory/Core/Common/Result.cs
@@ -0,0 +1,35 @@
+namespace Core.Common;
+
+public class Result
+{
+    public bool IsSuccess { get; }
+    public string Error { get; }
+    public bool IsFailure => !IsSuccess;
+
+    protected Result(bool isSuccess, string error)
+    {
+        if (isSuccess && error != string.Empty)
+            throw new InvalidOperationException("Success result cannot have an error message.");
+        if (!isSuccess && error == string.Empty)
+            throw new InvalidOperationException("Failure result must have an error message.");
+
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public static Result Success() => new(true, string.Empty);
+    public static Result Failure(string error) => new(false, error);
+
+    public static Result<T> Success<T>(T value) => new(value, true, string.Empty);
+    public static Result<T> Failure<T>(string error) => new(default, false, error);
+}
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    public Result(T? value, bool isSuccess, string error) : base(isSuccess, error)
+    {
+        Value = value;
+    }
+}
