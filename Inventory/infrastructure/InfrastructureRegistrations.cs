5c223acbce597f2eabb3d6aeb2c0ff873b66b269
mutassem-almahamid1 <mutassemalmahamid@gmail.com>
2026-04-06 04:50:33 +0300

    feat(Infrastructure): add repository registrations

--- Inventory/infrastructure/InfrastructureRegistrations.cs
+++ Inventory/infrastructure/InfrastructureRegistrations.cs
@@ -19,6 +19,13 @@ public static class InfrastructureRegistrations
     public static void AddRepositories(this IServiceCollection services)
     {
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IInventoryRepository, InventoryRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
 
         services.AddScoped<IIdentityService, IdentityService>();
         services.AddSingleton<IJwtTokenFactory, JwtTokenFactory>();
