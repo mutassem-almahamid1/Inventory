3399212ccad63b13362d1d954397bebb676afa8a
mutassem-almahamid1 <mutassemalmahamid@gmail.com>
2026-04-09 05:38:44 +0300

    feat(TransactionsController): add pagination to GetAll method

--- Inventory/Api/Controllers/TransactionsController.cs
+++ Inventory/Api/Controllers/TransactionsController.cs
@@ -17,9 +17,9 @@ namespace Api.Controllers;
 public class TransactionsController(ISender sender) : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult> GetAll()
+    public async Task<ActionResult> GetAll([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
     {
-        var result = await sender.Send(new GetAllTransactionsQuery());
+        var result = await sender.Send(new GetAllTransactionsQuery(pageNumber, pageSize));
         if (result.IsSuccess)
             return Ok(result.Value);
 
